2cf299953962d2df158bad334a6847dc36b32507
Patrick O'Brien <obrienp2321@gmail.com>
2023-07-16 10:16:25 +0200

    server made redundant due to cyclic built in cron jobs

--- server.js
+++ server.js
@@ -120,41 +120,6 @@ function keepServerActive() {
 
 keepServerActive()
 
-
-
-// function nodeMailerTest() {
-//   nodeCron.schedule("*/1 * * * *", () => {
-//     console.log("NodeMailerTest triggered");
-//     let date = new Date();
-
-//     let smtpTransporter = nodemailer.createTransport({
-//       service: "Gmail",
-//       port: 465,
-//       auth: {
-//         user: process.env.GMAIL_USER,
-//         pass: process.env.GMAIL_PASS,
-//       },
-//     });
-//     let mailOptions = {
-//       from: process.env.GMAIL_USER,
-//       to: process.env.GMAIL_USER,
-//       subject: `nodecronServer triggered at ${date}`,
-//       html: `
-//       <h3>Successful trigger at ${date}</h3>
-
-//      `,
-//     };
-  
-//     smtpTransporter.sendMail(mailOptions, (error) => {
-//       if (error) {
-//         console.error("Failed to send mail:", error);
-//       } else {
-//         console.log("Success email sent");
-//       }
-//     });
-//   });
-// }
-
 function nodeMailerConfirmationEmail(source, responseData) {
 
     console.log("NodeMailerTest triggered");
